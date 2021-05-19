e154a66d176c78afc87fa33da3bf3b27cfc2504f
Kseniia Latysheva <latysheva.ks@yandex.ru>
2021-05-19 15:40:31 +0300

    person class filled

--- src/main/java/de/telekom/sea2/model/Person.java
+++ src/main/java/de/telekom/sea2/model/Person.java
@@ -7,4 +7,58 @@ public class Person {
     private Salutation salutation;
     private String firstname;
     private String lastname;
+    private static long personsCounter = 0;
+
+    public Person(Salutation salutation, String lastname, String firstname) {
+        this.id = ++personsCounter;
+        this.salutation = salutation;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setSalutation(Salutation salutation) {
+        this.salutation = salutation;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public Salutation getSalutation() {
+        return salutation;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) // null check
+        {
+            return false;
+        }
+        if (this == obj) // self check
+        {
+            return true;
+        }
+        if (!(obj instanceof Person)) {// type check
+            return false;
+        }
+        Person person = (Person) obj; // cast to Person
+        return ((person.getId()==this.getId()) &&person.getFirstname().equals(this.firstname) && person.getLastname().equals(this.lastname) && person.getSalutation().equals(this.salutation));
+    }
 }
+
